974342f7e467012df2d8a110dd1437774594ac72
00960194 <edward.chapman.18@cnu.edu>
2021-02-06 15:49:50 -0500

    first push

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
-//#includes are ALWAYS at top of file
-#include <iostream>	//iostream is where std::cout and std::endl are defined
 
-std::string func()
-{
+#include <iostream>	//iostream is where std::cout and std::endl are defined
+#include "myfunc.h"
+// test change
+//std::string func()
+//{
 	//say hello
-	return "hello";
-}
+	//return "hello";
+//}
 
 int main()
 {
 	std::cout<<func()<<std::endl;
 	return 0;
 }
+
